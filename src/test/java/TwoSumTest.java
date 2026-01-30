61492390c94816d4717291913d41c36b9038648b
Andrei Kuzmin <Andreytko@mail.ru>
2026-01-30 17:38:28 +0300

    task 1

--- src/test/java/TwoSumTest.java
+++ src/test/java/TwoSumTest.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.Arrays;
-
 @ExtendWith(MockitoExtension.class)
 public class TwoSumTest {
 
