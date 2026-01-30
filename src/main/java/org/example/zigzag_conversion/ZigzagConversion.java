2bcfcf58ab0cb3474831b5f59e943d10ac143d46
Andrei Kuzmin <Andreytko@mail.ru>
2026-01-30 16:52:40 +0300

    task 6

---
+++ src/main/java/org/example/zigzag_conversion/ZigzagConversion.java
@@ -0,0 +1,38 @@
+package org.example.zigzag_conversion;
+
+public class ZigzagConversion {
+
+    public String convert(String s, int numRows) {
+        if (numRows == 1) {
+            return s;
+        }
+
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < numRows; i++) {
+
+            if (i == 0 || i == numRows - 1) {
+                for (int j = i; j < s.length(); j = j + (numRows - 1) * 2) {
+                    builder.append(s.charAt(j));
+                }
+            } else {
+                int j = i;
+                int step = 1;
+                while (j < s.length()) {
+                    builder.append(s.charAt(j));
+                    if (step % 2 == 1) {
+                        j = j + (numRows - 1) * 2 - 2 * i;
+                    } else {
+                        j = j + 2 * i;
+                    }
+                    step++;
+                }
+
+            }
+
+        }
+
+        return builder.toString();
+    }
+
+}
---
+++ src/test/java/ZigZagConversionTest.java
@@ -0,0 +1,5 @@
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(MockitoE)
+public class ZigZagConversionTest {
+}
