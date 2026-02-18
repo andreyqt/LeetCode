7103d58a745f162c7abce4f404aac84d66551d9a
Andrei Kuzmin <Andreytko@mail.ru>
2026-02-18 18:55:13 +0300

    peeking iterator

---
+++ src/main/java/org/example/iterator/PeekingIterator.java
@@ -0,0 +1,61 @@
+package org.example.iterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * task: <a href="https://leetcode.com/problems/peeking-iterator/description/">Peeking Iterator</a><br>
+ * time complexity: {@code O(1)} for all operations
+ * space complexity: {@code O(1)}
+ * difficulty: {@code medium}
+ */
+public class PeekingIterator implements Iterator<Integer> {
+
+    private Iterator<Integer> iterator;
+    private Integer nextInteger;
+    private boolean hasNext;
+
+    public PeekingIterator(Iterator<Integer> iterator) {
+        this.iterator = iterator;
+
+        if (iterator.hasNext()) {
+            nextInteger = iterator.next();
+            hasNext = true;
+        } else {
+            hasNext = false;
+            //nextInteger = null;
+        }
+    }
+
+    public Integer peek() {
+        if (hasNext) {
+            return nextInteger;
+        } else {
+            throw new NoSuchElementException();
+        }
+    }
+
+    @Override
+    public boolean hasNext() {
+        return hasNext;
+    }
+
+    @Override
+    public Integer next() {
+        if (!hasNext) {
+            throw new NoSuchElementException();
+        }
+
+        Integer next = nextInteger;
+
+        if (iterator.hasNext()) {
+            nextInteger = iterator.next();
+        } else {
+            hasNext = false;
+            nextInteger = null;
+        }
+
+        return next;
+    }
+
+}
