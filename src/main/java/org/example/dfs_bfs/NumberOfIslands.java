7799aa0bf0a871560f31de7d38fe1ba11d62bf11
Andrei Kuzmin <Andreytko@mail.ru>
2026-02-19 18:26:51 +0300

    task 200. number of islands, BFS

--- src/main/java/org/example/dfs_bfs/NumberOfIslands.java
+++ src/main/java/org/example/dfs_bfs/NumberOfIslands.java
@@ -1,5 +1,8 @@
 package org.example.dfs_bfs;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * task: <a href="https://leetcode.com/problems/number-of-islands/description/">Number of Islands</a><br>
  * time complexity: {@code O(M*N)}
@@ -26,6 +29,48 @@ public class NumberOfIslands {
             }
         }
 
+        return count;
+    }
+
+    public int numIslands2(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int count = 0;
+
+        int[][] directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+
+                    Queue<int[]> queue = new LinkedList<>();
+                    queue.add(new int[]{i, j});
+                    grid[i][j] = '0';
+
+                    while (!queue.isEmpty()) {
+                        int[] current = queue.poll();
+
+                        for (int[] direction : directions) {
+                            int newRow = current[0] + direction[0];
+                            int newCol = current[1] + direction[1];
+
+                            if (newRow >= 0 && newRow < rows &&
+                                newCol >= 0 && newCol < cols &&
+                                grid[newRow][newCol] == '1') {
+
+                                queue.offer(new int[]{newRow, newCol});
+                                grid[newRow][newCol] = '0';
+                            }
+                        }
+                    }
+                }
+            }
+        }
         return count;
     }
 
