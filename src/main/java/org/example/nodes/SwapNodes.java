8c45181778ed966519817b3fb3842f5604297a60
Andrei Kuzmin <Andreytko@mail.ru>
2026-02-22 00:48:38 +0300

    swap nodes

---
+++ src/main/java/org/example/nodes/SwapNodes.java
@@ -0,0 +1,32 @@
+package org.example.nodes;
+
+/**
+ * task: <a href="https://leetcode.com/problems/swap-nodes-in-pairs/description/">Swap Nodes in Pairs</a><br>
+ * time complexity: {@code O(N)}
+ * space complexity: {@code O(1)}
+ * difficulty: {@code easy}
+ */
+public class SwapNodes {
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode current = dummy;
+
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+
+            first.next = second.next;
+            second.next = first;
+            current.next = second;
+
+            current = first;
+        }
+
+        return dummy.next;
+    }
+
+}
