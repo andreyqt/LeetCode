16a2e3aaf698973f04d622d2acbc811e052bd521
Andrei Kuzmin <Andreytko@mail.ru>
2026-02-23 19:53:30 +0300

    path sum

--- src/main/java/org/example/tree/PathSum.java
+++ src/main/java/org/example/tree/PathSum.java
@@ -1,35 +1,23 @@
 package org.example.tree;
 
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * task: <a href="https://leetcode.com/problems/path-sum-ii/description/">Path Sum II</a><br>
- * time complexity: {@code O(N^2)}<br>
- * space complexity: {@code O(N*Log(N))}<br>
+ * task: <a href="https://leetcode.com/problems/path-sum/description/">Path Sum I</a><br>
+ * time complexity: {@code O(N)}<br>
+ * space complexity: {@code O(Log(N))}<br>
  * difficulty: {@code medium}
  */
 public class PathSum {
 
-    public List<List<Integer>> pathSum(MyTreeNode root, int sum) {
-        List<List<Integer>> result = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-        dfs(root, sum, current, result);
-        return result;
-    }
+    public boolean hasPathSum(MyTreeNode root, int targetSum) {
+        if (root == null) return false;
 
-    private void dfs(MyTreeNode node, int remainingSum, List<Integer> currentSum,
-                     List<List<Integer>> result) {
-        if (node == null) {return;}
-
-        currentSum.add(node.val);
-        if (node.left == null && node.right == null && remainingSum == node.val) {
-            result.add(new ArrayList<>(currentSum));
-        } else {
-            dfs(node.left, remainingSum - node.val, currentSum, result);
-            dfs(node.right, remainingSum - node.val, currentSum, result);
+        if (root.left == null && root.right == null) {
+            return root.val == targetSum;
         }
-        currentSum.remove(currentSum.size() - 1);
+
+        targetSum -= root.val;
+
+        return hasPathSum(root.left, targetSum) || hasPathSum(root.right, targetSum);
     }
 
 }
