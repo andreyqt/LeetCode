8143cd0da020860d2c78e96a33a072388f0963e5
Andrei Kuzmin <Andreytko@mail.ru>
2026-02-26 18:52:39 +0300

    1404. Number of Steps to Reduce a Number in Binary Representation to One

---
+++ src/main/java/org/example/binary/BinaryRepresentation.java
@@ -0,0 +1,59 @@
+package org.example.binary;
+
+/**
+ * task: <a href="https://leetcode.com/problems/number-of-steps-to-reduce-a-number-in-binary-representation-to-one/">Number of Steps to Reduce a Number in Binary Representation to One</a><br>
+ * time complexity: {@code O(N)}<br>
+ * space complexity: {@code O(1)}<br>
+ * difficulty: {@code medium}
+ */
+public class BinaryRepresentation {
+
+
+    public int numberOfSteps(String s) {
+        int steps = 0, carry = 0;
+
+        for (int i = s.length() - 1; i > 0; i--) {
+            int digit = (s.charAt(i) - '0') + carry;
+
+            if (digit == 0) {
+                steps += 1;
+                carry = 0;
+            } else if (digit == 1) {
+                steps += 2;
+                carry = 1;
+            } else {
+                steps += 1;
+                carry = 1;
+            }
+        }
+        return steps + carry;
+    }
+
+    //applicable for small sizes of s due to int (and even long) capacity
+    public int numberOfSteps2(String s) {
+        int num = convertToDecimal(s);
+        int stepCount = 0;
+        while (num != 1) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num += 1;
+            }
+            stepCount++;
+        }
+        return stepCount;
+    }
+
+    public int convertToDecimal(String binary) {
+        int res = 0;
+        for (int i = 0; i < binary.length(); i++) {
+            char c = binary.charAt(i);
+            if (c == '0') {
+                continue;
+            }
+            res = res + (int) Math.pow(2, binary.length() - i - 1);
+        }
+        return res;
+    }
+
+}
