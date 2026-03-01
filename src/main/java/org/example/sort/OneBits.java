7b7be86ffff911870d525fe4859cb698916f3da7
Andrei Kuzmin <Andreytko@mail.ru>
2026-03-01 14:16:27 +0300

    refactor

--- src/main/java/org/example/sort/OneBits.java
+++ src/main/java/org/example/sort/OneBits.java
@@ -1,10 +1,24 @@
 package org.example.sort;
 
+import java.util.Arrays;
+
 /**
  * task: <a href="https://leetcode.com/problems/remove-duplicates-from-sorted-array/">RemoveElement Duplicates from Sorted Array</a><br>
- * time complexity: {@code O(N)}<br>
- * space complexity: {@code O(1)}<br>
+ * time complexity: {@code O(N*Log(N))}<br>
+ * space complexity: {@code O(N)}<br>
  * difficulty: {@code easy}
  */
 public class OneBits {
+
+    public int[] sortByOneBits(int[] arr) {
+        return Arrays.stream(arr)
+                .boxed()
+                .sorted((a,b)->{
+                    int bitCompare = Integer.bitCount(a) - Integer.bitCount(b);
+                    return bitCompare == 0 ? a - b : bitCompare;
+                })
+                .mapToInt(Integer::intValue)
+                .toArray();
+    }
+
 }
