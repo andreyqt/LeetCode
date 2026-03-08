3fede79c75b12d5fceb8017f44109cdd749c6160
Andrei Kuzmin <Andreytko@mail.ru>
2026-03-08 16:23:55 +0300

    1980. Find Unique Binary String

---
+++ src/main/java/org/example/string_tasks/FindUniqueBinString.java
@@ -0,0 +1,21 @@
+package org.example.string_tasks;
+
+/**
+ * task: <a href="https://leetcode.com/problems/find-unique-binary-string/">Find Unique Binary String</a><br>
+ * time complexity: {@code O()}<br>
+ * space complexity: {@code O()}<br>
+ * difficulty: {@code medium}
+ */
+public class FindUniqueBinString {
+
+    public String findDifferentBinaryString(String[] nums) {
+        int n = nums.length;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            char ch = nums[i].charAt(i);
+            sb.append((char) '1' - ch);
+        }
+        return sb.toString();
+    }
+
+}
