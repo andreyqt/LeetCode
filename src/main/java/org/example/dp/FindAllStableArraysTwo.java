4b55ce28b93df483b1f1476ffb98bb2605602e65
Andrei Kuzmin <Andreytko@mail.ru>
2026-03-10 19:37:41 +0300

    3130. Find All Possible Stable Binary Arrays I

--- src/main/java/org/example/dp/FindAllStableArraysTwo.java
+++ src/main/java/org/example/dp/FindAllStableArraysTwo.java
@@ -1,12 +1,12 @@
 package org.example.dp;
 
 /**
- * task: <a href="https://leetcode.com/problems/find-all-possible-stable-binary-arrays-i/">Find All Possible Stable Binary Arrays I</a><br>
+ * task: <a href="https://leetcode.com/problems/find-all-possible-stable-binary-arrays-ii/">Find All Possible Stable Binary Arrays II</a><br>
  * time complexity: {@code O(Z * O)}<br>
  * space complexity: {@code O(Z * O)}<br>
- * difficulty: {@code medium}
+ * difficulty: {@code hard}
  */
-public class FindAllStableArrays {
+public class FindAllStableArraysTwo {
 
     public int numberOfStableArrays(int zero, int one, int limit) {
         int mod = 1_000_000_007;
@@ -23,8 +23,8 @@ public class FindAllStableArrays {
         for (int i = 1; i <= zero; i++) {
             for (int j = 1; j <= one; j++) {
 
-                long last0 = (i - limit >= 1) ? dp[i - limit - 1][j][1] : 0;
-                long last1 = (j - limit >= 1) ? dp[i][j - limit - 1][0] : 0;
+                long last0 = (i - limit - 1 >= 0) ? dp[i - limit - 1][j][1] : 0;
+                long last1 = (j - limit - 1 >= 0) ? dp[i][j - limit - 1][0] : 0;
 
                 dp[i][j][0] = (dp[i - 1][j][0] + dp[i - 1][j][1] - last0 + mod) % mod;
                 dp[i][j][1] = (dp[i][j - 1][0] + dp[i][j - 1][1] - last1 + mod) % mod;
