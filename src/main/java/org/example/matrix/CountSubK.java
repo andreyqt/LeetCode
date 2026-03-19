bc690e89499925a663a8ffbcad8de0143d13b736
Andrei Kuzmin <Andreytko@mail.ru>
2026-03-20 00:04:58 +0300

    Count Submatrices With Equal Frequency of X and Y

--- src/main/java/org/example/matrix/CountSubK.java
+++ src/main/java/org/example/matrix/CountSubK.java
@@ -2,12 +2,14 @@ package org.example.matrix;
 
 /**
  * task: <a href="https://leetcode.com/problems/count-submatrices-with-top-left-element-and-sum-less-than-k/">Count Submatrices with Top-Left Element and Sum Less Than k</a><br>
- * time complexity: {@code O(n * m)}<br>
- * space complexity: {@code O(m * n)}<br>
  * difficulty: {@code medium}
  */
 public class CountSubK {
 
+    /**
+     * time complexity: {@code O(n * m)}<br>
+     * space complexity: {@code O(m * n)}<br>
+     */
     public int countSubmatrices(int[][] grid, int k) {
         int count = 0;
         int n = grid.length, m = grid[0].length;
@@ -30,6 +32,27 @@ public class CountSubK {
             }
         }
 
+        return count;
+    }
+
+    /**
+     * time complexity: {@code O(n * m)}<br>
+     * space complexity: {@code O(m)}<br>
+     */
+    public int countSubmatrices2(int[][] grid, int k) {
+        int count = 0;
+        int n = grid.length, m = grid[0].length;
+        int[] rowSum = new int[m];
+
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = 0; j < m; j++) {
+                rowSum[j] += grid[i][j];
+                sum += rowSum[j];
+                if (sum <= k) count++;
+                else break;
+            }
+        }
         return count;
     }
 
